e9f3104422dee90b9d8b4defd93a749e8a949e2a
Fabi-Boniolo <fabianaboniolo@gmail.com>
2024-02-16 14:57:56 -0300

    Criado componente de imagem

---
+++ pages/login.jsx
@@ -0,0 +1,11 @@
+import ImageWidthSpace from "../src/components/layout/ImageWidhtSpace"
+
+function LoginPage() {
+    return (
+        <ImageWidthSpace>
+            Teste
+        </ImageWidthSpace>
+    )
+}
+
+export default LoginPage
